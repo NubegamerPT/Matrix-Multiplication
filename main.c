0e0b9c8e0f15b71f73a1743a21536199bbdba3c8
NubegamerPT <145158518+NubegamerPT@users.noreply.github.com>
2024-03-19 17:32:28 +0000

    Added base

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+
+#define ANSI_COLOR_RED     "\033[0;31m" //Console RED
+#define ANSI_COLOR_GREEN   "\033[0;32m" //Console GREEN
+#define ANSI_COLOR_YELLOW  "\033[0;33m" //Console YELLOW
+#define ANSI_COLOR_BLUE    "\033[0;34m" //Console BLUE
+#define ANSI_COLOR_MAGENTA "\033[0;35m" //Console MAGENTA
+#define ANSI_COLOR_CYAN    "\033[0;36m" //Console CYAN
+#define ANSI_COLOR_RESET   "\033[0m"    /* Reset to default color */
+
+int main()
+{
+    return 0;
+}
