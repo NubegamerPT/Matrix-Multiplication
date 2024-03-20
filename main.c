ecb933667a86527bce4f99b6768345e274b9f567
Nubegamer_PT <afonsoclerigomendessousa@gmail.com>
2024-03-20 04:24:01 +0000

    Added support to Mac/Linux

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <ctype.h>
 #include <math.h>
+#include <string.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+    #define SLEEP_MS(milliseconds) Sleep(milliseconds)
+#elif defined(__APPLE__) || defined(__MACH__) || defined(__linux__)
+    #include <unistd.h>
+    #define SLEEP_MS(milliseconds) usleep(milliseconds * 1000)
+#else
+    #error "Unsupported operating system"
+#endif
+
 
 #include "functions.c"
 
@@ -17,8 +28,14 @@ int main()
     // cols ->, rows ↓
 
     // Define 3D vectors
-    int arrayTest1[ARRAY_ROWS_A][ARRAY_COLS_A];
-    int arrayTest2[ARRAY_ROWS_B][ARRAY_COLS_B];
+    writePlus(0,"TESTE");
+    writePlus(1,"TESTE");
+    writePlus(2,"TESTE");
+    writePlus(3,"TESTE");
+    writePlus(4,"TESTE");
+    SLEEP_MS(1000);
+    /* int arrayTest1[MAX_ARRAY][MAX_ARRAY];
+    int arrayTest2[MAX_ARRAY][MAX_ARRAY];
 
     if (checkArray(ARRAY_COLS_A, ARRAY_ROWS_B) == -1)
     {
@@ -27,7 +44,10 @@ int main()
     else
     {
         writePlus(3, "Matrices are suitable for multiplication!");
-    }
-
+        requestArray(arrayTest1,ARRAY_ROWS_A,ARRAY_COLS_A);
+        dysplayArray(arrayTest1,ARRAY_ROWS_A,ARRAY_COLS_A);
+        requestArray(arrayTest2,ARRAY_ROWS_B,ARRAY_COLS_B);
+        dysplayArray(arrayTest2,ARRAY_ROWS_B,ARRAY_COLS_B);
+    } */
     return 0;
 }
