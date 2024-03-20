44add8d9872ffac5db1bec72953538183b99e91e
Nubegamer_PT <afonsoclerigomendessousa@gmail.com>
2024-03-20 04:23:38 +0000

    Contains essential functions

--- functions.c
+++ functions.c
@@ -4,13 +4,12 @@
 #include <ctype.h>
 #include <math.h>
 
-#define ANSI_COLOR_RED "\033[0;31m"     // Console RED
-#define ANSI_COLOR_GREEN "\033[0;32m"   // Console GREEN
-#define ANSI_COLOR_YELLOW "\033[0;33m"  // Console YELLOW
-#define ANSI_COLOR_BLUE "\033[0;34m"    // Console BLUE
-#define ANSI_COLOR_MAGENTA "\033[0;35m" // Console MAGENTA
-#define ANSI_COLOR_CYAN "\033[0;36m"    // Console CYAN
-#define ANSI_COLOR_RESET "\033[0m"      // Reset to default color
+#define ANSI_COLOR_RED "\x1b[91m" // Console RED
+#define ANSI_COLOR_GREEN "\x1b[32m" // Console GREEN
+#define ANSI_COLOR_YELLOW "\x1b[33m" // Console YELLOW
+#define ANSI_COLOR_BLUE "\x1b[34m" // Console BLUE
+#define ANSI_COLOR_WHITE "\x1b[37m" // Console WHITE
+#define ANSI_COLOR_RESET "\x1b[0m" // Console RESET
 
 #define MAX_STRING_SIZE 256
 #define MAX_ARRAY 64
@@ -26,10 +25,11 @@ int checkArray(int collomsA, int linesB)
 }
 
 int writePlus(int type, const char text[MAX_STRING_SIZE])
-{   
-    //this function allows the programer to easially send printf with color code,
-    //for exemple, writePlus(2, Files deleted!), the  output will be: "(in red)[ERROR] Files deleted!"
+{
+    // this function allows the programer to easially send printf with color code,
+    // for exemple, writePlus(2, Files deleted!), the  output will be: "(in red)[ERROR] Files deleted!"
     int stat = 0;
+    printf(ANSI_COLOR_RESET "");
     switch (type)
     {
     case 0:
@@ -42,16 +42,44 @@ int writePlus(int type, const char text[MAX_STRING_SIZE])
         stat = printf(ANSI_COLOR_RED "[ERRO] %s", text); // text with red color
         break;
     case 3:
-        stat = printf(ANSI_COLOR_GREEN "[SUCESS] %s", text); // text with no color
+        stat = printf(ANSI_COLOR_GREEN "[SUCESS] %s", text); // text with green color
         break;
     case 4:
-        stat = printf(ANSI_COLOR_BLUE "[OUTPUT] %s", text); // text with no color
+        stat = printf(ANSI_COLOR_BLUE "[OUTPUT] %s", text); // text with blue color
+        break;
+    case 5:
+        stat = printf("[INPUT] %s", text); // text with no color (white)
         break;
     default:
         stat = printf("[INFO] %s", text); // text with no color (white)
-        break;
     }
-    printf(ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_RESET "");
+    printf("\n");
     return stat;
 }
 
+void requestArray(int userArray[MAX_ARRAY][MAX_ARRAY], int arrayRows, int arrayCols)
+{
+    printf("[INFO] Insert the elements of array: \n");
+    for (int row = 0; row < arrayRows; row++)
+    {
+        for (int col = 0; col < arrayCols; col++)
+        {
+            printf("[INPUT] Element [%d,%d]: ", row, col);
+            scanf("%d", &userArray[row][col]);
+        }
+    }
+}
+
+void dysplayArray(int userArray[MAX_ARRAY][MAX_ARRAY], int arrayRows, int arrayCols)
+{
+    writePlus(0, "The array you inserted is:");
+    for (int row = 0; row < arrayRows; row++)
+    {
+        for (int col = 0; col < arrayCols; col++)
+        {
+            printf("[%d]", userArray[row][col]);
+        }
+        printf("\n");
+    }
+}
