b983ce8d78edb39390b341399088f4108eb9a5d3
Nubegamer_PT <afonsoclerigomendessousa@gmail.com>
2024-03-29 01:56:41 +0000

    The code core

--- main.c
+++ main.c
@@ -21,6 +21,11 @@ int main()
     int resultSizeCols = 0;
     int resultSizeRows = 0;
 
+    //temp's are a "holder" to be used in conditions
+    int tempA = 0;
+    int tempB = 0;
+    int tempArray[MAX_ARRAY] = {0};
+
     // gives sizes for 2 matrixes used for testting porpuses
     // cols ->, rows ↓
 
@@ -45,34 +50,67 @@ int main()
     }
     else
     {
-        resultSizeCols = colsA;
-        resultSizeRows = rowsB;
+        resultSizeCols = colsB;
+        resultSizeRows = rowsA;
         writePlus(3, "Matrices are suitable for multiplication!");
-        requestArray(array1, colsA, rowsA);
-        dysplayArray(array1, colsA, rowsA, "The array you inserted:");
-        requestArray(array2, colsB, rowsB);
-        dysplayArray(array2, colsB, rowsB, "The array you inserted:");
-    }
+        requestArray(array1, rowsA, colsA);
+        dysplayArray(array1, rowsA, colsA, "The array you inserted:");
+        requestArray(array2, rowsB, colsB);
+        dysplayArray(array2, rowsB, colsB, "The array you inserted:");
 
-//  for (int colsA_ = 0; colsA_ < colsA; colsA_++)
-//    { 
         for (int rowsA_ = 0; rowsA_ < rowsA; rowsA_++)
         {
             for (int colsB_ = 0; colsB_ < colsB; colsB_++)
             {
                 for (int rowsB_ = 0; rowsB_ < rowsB; rowsB_++)
                 {
-                    arrayResult[rowsA_][colsB_] += array1[rowsA_][rowsB_] * array2[rowsB_][colsB_];
-                    printf("Doing: [%d][%d] += [%d][%d]*[%d][%d]   ", rowsA_, colsB_, rowsA_, rowsB_, rowsB_, colsB_);
-                    printf("colsA_ = [%d] rowsA_ = [%d] colsB_ = [%d] rowsB_ = [%d]\n", 0, rowsA_, colsB_, rowsB_);
-                    SLEEP_MS(1);
+                    tempArray[rowsB_] = rowsB_;
+
+                    if (colsB_ == 0 && rowsB_ == 0)
+                    {
+                        tempA = 0;
+                    }
+                    else if (colsB_ > 0 && rowsB_ == 0)
+                    {
+                        tempB = tempA;
+                        tempA = 0;
+                    }
+                    else if (rowsB_ > 0)
+                    {
+                        tempA = rowsB_;
+                    }
+                    else if (colsB_ > 0)
+                    {
+                        tempA = colsB_;
+                    }
+
+                    if (colsB_ == 0 && rowsB_ == 0)
+                    {
+                        tempB = 0;
+                    }
+                    else if (colsB_ > 0 || rowsB_ > 0)
+                    {
+                        tempB = colsB_;
+                    }
+
+                    arrayResult[rowsA_][colsB_] += array1[rowsA_][rowsB_] * array2[tempA][tempB];
+
+                    //used for debugging
+                    /*printf("Doing: [%d][%d] += [%d][%d] = %2d * [%d][%d] = %2d  ", rowsA_, colsB_, rowsA_, rowsB_, array1[rowsA_][rowsB_], tempA, tempB, array2[tempA][tempB]);
+                    printf("tempA = [%d] tempB = [%d] || rowsA_ = [%d] colsB_ = [%d] rowsB_ = [%d]\n", tempA, tempB, rowsA_, colsB_, rowsB_);*/ 
+                    printf("[INFO]    Doing: [%d][%d] += [%d][%d] = %2d * [%d][%d] = %2d  \n", rowsA_ + 1, colsB_ + 1, rowsA_ + 1, colsB_ + 1, array1[rowsA_][rowsB_], tempA+1, tempB+1, array2[tempA][tempB]);
+                    SLEEP_MS(0.5);
                 }
             }
         }
-//    }
-
-    dysplayArray(arrayResult, resultSizeCols, resultSizeRows, "The resulting array will look like this:");
-    SLEEP_MS(1000000);
+        dysplayArray(arrayResult, resultSizeRows, resultSizeCols, "The resulting array will look like this:");
+        SLEEP_MS(5000);
+    }
+    fflush(stdin);
+    writePlus(1,"Press any key to leave the program.");
+    getchar();
+    writePlus(0,"Exiting the program now...");
+    SLEEP_MS(3000);
 
     return 0;
 }
